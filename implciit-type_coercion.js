a4afaec26d4f2d799592d7c3c5f0f92433ef2214
davidajimati <ajimatidavid@gmail.com>
2023-04-04 17:09:01 +0100

    implicit type coercion

---
+++ implciit-type_coercion.js
@@ -0,0 +1,10 @@
+// there are 2 types
+// implicit -> javascript
+// explicit -> done by the programmer
+
+str = 'hello'
+num = 5
+sum  = num + str
+console.log(typeof(sum))
+
+console.log(null + str)
--- playground.js
+++ playground.js
@@ -1,2 +1,10 @@
-console.log("a" * 3);
-// NaN
+var n = 2;
+var p = 4;
+var q = 5;
+var w = 3;
+if ( !((p * q) /n <= (q * w) + n/p )) {
+console.log( ++p + w++ + " " + ++n);
+}
+else {
+console.log(--p + q-- + " " + --n);
+}
