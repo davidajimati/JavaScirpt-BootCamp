bc5af7b41b5ff097132c46511df15dbf044b31c8
davidajimati <ajimatidavid@gmail.com>
2023-04-05 13:23:33 +0100

    string  methods

---
+++ string-methods.js
@@ -0,0 +1,11 @@
+let userName = "David"
+// console.log(userName.length)
+// console.log(userName.charAt(0))
+// console.log(userName.indexOf('i'))
+// console.log(userName.lastIndexOf('i'))
+// console.log(userName.trim())
+// console.log(userName.toLowerCase())
+// console.log(userName.toUpperCase())
+
+userName = userName.replaceAll(' ', '-')
+console.log(userName)
