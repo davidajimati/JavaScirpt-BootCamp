1fdff854f42ed9bb677f5174b3d06cca98c4c387
davidajimati <ajimatidavid@gmail.com>
2023-04-07 16:50:35 +0100

    google temperature converter updated

--- mini-projects/temperature-converter/googles-clone/version2.js
+++ mini-projects/temperature-converter/googles-clone/version2.js
@@ -1,23 +1,25 @@
-let far = Number(document.getElementById("celsius").innerHTML)
+// function updateBoxFar() {
+//     const boxCelsius = document.getElementById("celsius")
+//     const boxFar = document.getElementById("far")
+//     boxFar.value = (boxCelsius - 32) * (5 / 9)
+// }
+
+// updateBoxCelsius
+
+// function updateBoxCelsius() {
+//     const boxCelsius = document.getElementById("celsius")
+//     const boxFar = document.getElementById("far")
+//     boxCelsius.value = (num * 9 / 5) + 32
+// }
 
 
-document.getElementById("celsius").innerHTML = toCelsius(fahren);
+const boxCelsius = document.getElementById("celsius");
+const boxFar = document.getElementById("far");
 
+boxCelsius.addEventListener("input", () => {
+    boxFar.value = (boxCelsius.value * 9 / 5) + 32;
+});
 
-document.getElementById("celsius").onclick = function() {
-    let celsius = Number(document.getElementById("celsius").innerHTML)
-    document.getElementById("far").innerHTML = toFar(celsius);
-}
-
-document.getElementById("far").onclick = function() {
-    let far = Number(document.getElementById("far").innerHTML)
-    document.getElementById("celsius").innerHTML = toFar(far);
-}
-
-function toCelsius(far) {
-    return (far - 32) * 5 / 9;
-}
-
-function toFar(celsius) {
-    return ((num * 9 / 5) + 32);
-}
+boxFar.addEventListener("input", () => {
+    boxCelsius.value = (boxFar.value - 32) * (5 / 9);
+});
