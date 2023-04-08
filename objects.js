0605630103ce298c8534a12f05e33ea8305d49db
davidajimati <ajimatidavid@gmail.com>
2023-04-08 22:15:14 +0100

    Introduction to javascript objects

---
+++ objects.js
@@ -0,0 +1,13 @@
+const car = {
+    model: "mustang",
+    color: "red",
+    year: 2023,
+
+    drive: () => console.log("You drive the car"),
+    brake: () => console.log("You stepped on the break")
+}
+
+console.log(car.color);
+car.drive();
+
+// this is just an introduction to objects
