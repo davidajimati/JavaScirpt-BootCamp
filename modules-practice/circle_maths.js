976f1c34615f9dac7f88587262893c061c9d365b
davidajimati <ajimatidavid@gmail.com>
2023-05-20 13:36:08 +0100

    module

---
+++ modules-practice/circle_maths.js
@@ -0,0 +1,13 @@
+PI = Math.PI;
+
+export function circumference(radius) {
+  return 2 * PI * radius;
+}
+
+export function Area(radius) {
+  return PI * radius * radius;
+}
+
+export function diameter (radius) {
+  return radius * 2;
+}
---
+++ modules-practice/index.js
@@ -0,0 +1,7 @@
+// import {PI, diameter, circumference, Area} from "./circle_maths.js"
+import * as MathCircle from "./circle_maths.js"
+CircleMath
+console.log(MathCircle.PI)
+console.log("circumference of circle" + MathCircle.circumference(10));
+console.log("area of circle" + MathCircle.Area(10));
+console.log("Diameter of circle" + MathCircle.diameter(10));
