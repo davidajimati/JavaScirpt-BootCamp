5656dc1987e90f037445256b33b3fc241bc11195
davidajimati <ajimatidavid@gmail.com>
2023-05-20 14:50:26 +0100

    DOM mnaipulation

---
+++ DOM/DOMindex.js
@@ -0,0 +1,10 @@
+// variable declarations to hasten manipulation
+
+let elem_id = document.getElementById;
+let elem_class = document.getElementsByClassName;
+let body_style = document.body.style;
+
+body_style.backgroundColor = "aqua";
+body_style.color = "red";
+body_style.fontSize = "40px";
+console.dir(document)
