829921c0c3f392dc7fd26f19ab2c9f70ebc84889
davidajimati <ajimatidavid@gmail.com>
2023-05-20 21:02:51 +0100

    DOM traversal

--- DOM/dom-index2.js
+++ DOM/dom-index2.js
@@ -1,4 +1,26 @@
+/**
+ * Options available for DOM traversal
+ * .firstElementChild
+ * .lastElementChild
+ * .parentElement
+ * .nextElementSibling
+ * .previousElementSibling
+ * .children[]
+ * Array.from(.children)
+ */
+
 let body = document.body;
-let veggies = document.getElementById("veggies");
-let child = body.firstElementChild;
-child.style.backgroundColor = "lightgreen";
+let veggies =  document.getElementById("veggies");
+// let child = body.firstElementChild;
+// child.style.backgroundColor = "lightgreen";
+
+// let parent = veggies.parentElement;
+// parent.style.backgroundColor = "pink";
+
+let next = veggies.nextElementSibling;
+next.style.backgroundColor = "yellow";
+
+// dessert_one = next.firstElementChild; OR
+dessert_one = next.children;
+dessert_one.style.color = "purple";
+dessert_one.style.backgroundColor = "lightgreen";
