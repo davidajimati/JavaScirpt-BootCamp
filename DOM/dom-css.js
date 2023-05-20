2ff5b86403c71fa4e59d67ca1fed229b452f4f6c
davidajimati <ajimatidavid@gmail.com>
2023-05-20 22:00:24 +0100

    CSS manipulation - DOM

---
+++ DOM/dom-css.js
@@ -0,0 +1,14 @@
+/**
+ * This script changes CSS properties of elements
+ *  using JavaScript
+ */
+
+const heading = document.querySelector("#heading").style;
+
+heading.color = "rgb(45, 36, 89)";
+heading.fontFamily = "courier"
+heading.border = "2px solid"
+heading.borderRadius = "20px"
+heading.backgroundColor = "lightgreen";
+heading.display = "flex"
+heading.justifyContent = "center"
