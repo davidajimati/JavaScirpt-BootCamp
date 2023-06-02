3b085f30d4e31db9edc9b7d709bcfddd4c0efa8f
davidajimati <ajimatidavid@gmail.com>
2023-06-02 16:26:33 +0100

    JQUERY lessons

---
+++ linkedin.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+Simple Intro. Clocking in!
+"""
+
+def check_trait(name):
+    if name == "David M. Ajimati":
+        print("This is a Great man\nLook no further!")
+        return
+
+if __name__ == "__main__":
+    """ Maranatha """
+    check_trait("David M. Ajimati")
